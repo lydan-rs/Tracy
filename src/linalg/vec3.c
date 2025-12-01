1e3fb5020a3736f4859bebb1d2faabe52e41d13d
lydan-rs <104039346+lydan-rs@users.noreply.github.com>
2025-12-01 16:21:23 +1100

    Adding extra functions to the Vec3 type.

--- src/linalg/vec3.c
+++ src/linalg/vec3.c
@@ -1,5 +1,57 @@
 #include "vec3.h"
+#include <math.h>
 
 float dotV3(Vec3 a, Vec3 b) {
 	return (a.x*b.x) + (a.y*b.y) + (a.z*b.z);
 }
+
+
+Vec3 addV3(Vec3 a, Vec3 b) {
+	return (Vec3){
+		.x = a.x+b.x,
+		.y = a.y+b.y,
+		.z = a.z+b.z
+	};
+}
+
+Vec3 subtract(Vec3 a, Vec3 b) {
+	return (Vec3){
+		.x = a.x-b.x,
+		.y = a.y-b.y,
+		.z = a.z-b.z
+	};
+}
+
+Vec3 scaleV3(float scalar, Vec3 v) {
+	return (Vec3){
+		.x = v.x*scalar,
+		.y = v.y*scalar,
+		.z = v.z*scalar
+	};
+}
+
+float magSqrdV3(Vec3 v) {
+	return dotV3(v, v);
+}
+
+float magnitudeV3(Vec3 v) {
+	return sqrtf(magSqrdV3(v));
+}
+
+Vec3 normalise(Vec3 v) {
+	const float mag = magnitudeV3(v);
+	return (Vec3){
+		.x = v.x/mag,
+		.y = v.y/mag,
+		.z = v.z/mag
+	};
+}
+
+Vec3 crossProdV3(Vec3 a, Vec3 b) {
+	return (Vec3){
+		.x = (a.y*b.z)-(a.z*b.y),
+		.y = (a.z*b.x)-(a.x*b.z),
+		.z = (a.x*b.y)-(a.y-b.x)
+	};
+}
+
--- src/linalg/vec3.h
+++ src/linalg/vec3.h
@@ -9,6 +9,13 @@ typedef struct {
 	};
 } Vec3;
 
-float dotV3(Vec3 a, Vec3 b);
+float  dotV3       (Vec3 a, Vec3 b);
+Vec3   addV3       (Vec3 a, Vec3 b);
+Vec3   subtract    (Vec3 a, Vec3 b);
+Vec3   scaleV3     (float scalar, Vec3 v);
+float  magSqrdV3   (Vec3 v);
+float  magnitudeV3 (Vec3 v);
+Vec3   normalise   (Vec3 v);
+Vec3   crossProdV3 (Vec3 a, Vec3 b);
 
 #endif
