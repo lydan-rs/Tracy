feff896fddca7219a8ca89022b172716d3f744d2
lydan-rs <104039346+lydan-rs@users.noreply.github.com>
2025-12-06 21:25:36 +1100

    Optimisation for sphere intersection.

--- src/main.c
+++ src/main.c
@@ -20,13 +20,20 @@ const int IMG_HEIGHT = 480;
 
 
 
-Vec3 rayColor(Ray r) {
+Vec3 skyboxColor(Ray r) {
 	float a = 0.5 * (r.direction.y + 1.0);
 	return addV3(
 			scaleV3((Vec3){1.0,1.0,1.0}, 1.0-a),
 			scaleV3((Vec3){0.5,0.7,1.0}, a)
 			);
 }
+
+typedef struct {
+	Vec3 surfacePoint;
+	Vec3 surfaceNormal;
+} Intersection;
+
+Intersecton
 
 
 
@@ -87,18 +94,26 @@ int main() {
 		for (int x = 0; x < image->width; x++) {
 			Ray ray = rays[INDEX_2D(x, y, image->width)];
 
-			Vec3  u = subtractV3(sphereCenter, ray.origin);
-			float a = dotV3(ray.direction, ray.direction);
-			float b = dotV3(scaleV3(ray.direction, -2.0), u);
-			float c = dotV3(u, u) - 1.0; // Radius is hardcoded to one here.
-			
-			float discriminant = b*b - 4*a*c;
+			// Vec3  u = subtractV3(sphereCenter, ray.origin);
+			// float a = dotV3(ray.direction, ray.direction);
+			// // float b = dotV3(scaleV3(ray.direction, -2.0), u);
+			// float b = -2.0 * dotV3(ray.direction, u);
+			// float c = dotV3(u, u) - 1.0; // Radius is hardcoded to one here.
+			// float discriminant = b*b - 4*a*c;
 
-			Vec3 outColor = rayColor(ray);
+			Vec3  u = subtractV3(sphereCenter, ray.origin);
+			float a = magSqrdV3(ray.direction);
+			float h = dotV3(ray.direction, u); // Aparently an optimisation?
+			float c = magSqrdV3(u) - 1.0; // Radius is hardcarded to one here.
+			float discriminant = h*h - a*c; // And another optimisation??
+			
+
+			Vec3 outColor = skyboxColor(ray);
 
 			if (discriminant >= 0.0) {
 				float discSqrt = sqrtf(discriminant);
-				float t = (-b - discSqrt) / (2.0*a);
+				// float t = (-b - discSqrt) / (2.0*a);
+				float t = (h-discSqrt)/a; // Again, an optimisation.
 
 				if (t > 0.0) {
 					outColor = normaliseV3(subtractV3(pointAlongRay(ray, t), sphereCenter)); // Surface normal
